f1f7b4f44c644f5c9d9bac6d8dd7e7009658dda5
João Netto <hi@joaonetto.me>
2014-08-04 16:00:37 -0300

    Finishing job

---
+++ ato1.md
@@ -0,0 +1,33 @@
+<!-- - Permitir na página de suporte que as imagens da galeria sejam links para os sites (configurado no media 
+uploader); -->
+<!-- - Alinhamento vertical das imagens -->
+
+<!-- - Carousel na home -->
+
+<!-- - Mudar plugin de lightbox para galeria -->
+
+<!-- - Vídeos no conteúdo manter 100% de largura -->
+
+
+<!-- Slides na home; -->
+
+<!-- Mudar formato na exibição do zoom  -->
+
+<!-- - Ainda em Suporte, colocar as imagens em colunas do grid e aplicar img-responsive dentro delas -->
+
+<!-- - Links:
+  Vimeo: vimeo.com/user18229685
+  Facebook: facebook.com/ -->
+
+<!-- - Colocar no cadastro de trabalhos um editor WYSIWYG chamado Pré-Galeria ou Conteúdo Pré-Galeria (opcional)
+  Quando preenchido, exibe antes de galeria e conteúdo
+  Nova estrutura da página:
+    - Titulo
+    - Subtitulo
+    - Pré-Conteúdo
+    - Galeria
+    - Conteúdo -->
+
+<!-- - Título de works está cinza (por conta do layout) -->
+
+<!-- - Sempre que usar a fonte Barbedor, exibir em caixa alta -->
--- templates/content.php
+++ templates/content.php
@@ -6,11 +6,64 @@
 <?php the_field('pre-content'); ?>
 
 <div class="portfolio-info">
-    <hr>
-    <?php if( function_exists( 'easy_image_gallery' ) ) { echo easy_image_gallery(); remove_filter( 'the_content', 'easy_image_gallery_append_to_content' );   } ?>
+    <?php
+    if(function_exists('easy_image_gallery')):
+      $attachment_ids = get_post_meta( $post->ID, '_easy_image_gallery', true );
+      $attachment_ids = array_filter(explode( ',', $attachment_ids ));
+
+      if ($attachment_ids) {
+        $has_gallery_images = get_post_meta( get_the_ID(), '_easy_image_gallery', true );
+        $has_gallery_images = explode( ',', get_post_meta( get_the_ID(), '_easy_image_gallery', true ) );
+
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'feature' );
+        $image_title = esc_attr( get_the_title( get_post_thumbnail_id( $post->ID ) ) );
+        ?>
+<hr>
+        <div class="row portfolio-images">
+        <?php
+          foreach ( $attachment_ids as $attachment_id ) {
+            $classes = array( '' );
+
+            // get original image
+            $image_link = wp_get_attachment_image_src( $attachment_id, apply_filters( 'easy_image_gallery_linked_image_size', 'large' ) );
+            $image_link = $image_link[0];
+
+            $image = wp_get_attachment_image( $attachment_id, apply_filters( 'easy_image_gallery_thumbnail_image_size', 'medium' ), '', array( 'alt' => trim( strip_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ) ), 'class' => 'img-responsive' ) );
+
+            $image_caption = get_post( $attachment_id )->post_excerpt ? get_post( $attachment_id )->post_excerpt : '';
+
+            $image_class = esc_attr( implode( ' ', $classes ) );
+
+            $settings = (array) get_option( 'easy-image-gallery' );
+
+            // set fancybox as default for when the settings page hasn't been saved
+            // $lightbox = isset( $settings['lightbox'] ) ? esc_attr( $settings['lightbox'] ) : 'prettyphoto';
+
+            $rel = ' data-lightbox="group"';
+
+            // $image_description = get_post($attachment_id)->post_content ? get_post($attachment_id)->post_content : '' ;
+
+        $html = sprintf( '<a %s href="%s" class="col-xs-6 col-sm-3 %s" title="%s">%s</a>', $rel, $image_link, $image_class, $image_caption, $image );
+
+      echo apply_filters( 'easy_image_gallery_html', $html, $rel, $image_link, $image_class, $image_caption, $image, $attachment_id, $post->ID );
+    }
+?>
+    </div>
+    <?php
+  }
+    endif;
+  ?>
     <hr>
 
     <?php the_content(); ?>
 
 
 </div>
+
+<style>
+  .single-works .content .portfolio-info hr { margin-top: 20px; }
+  .portfolio-images a { margin-bottom: 20px; }
+</style>
+
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js"></script>
